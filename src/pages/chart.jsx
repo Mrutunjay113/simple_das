2db78245de29e0328cc8f09bb91e8335cb55e56d
Mrutunjay113 <mrutunjay765@gmail.com>
2023-08-19 15:53:27 +0530

    fixed some bugs in chart

--- src/pages/chart.jsx
+++ src/pages/chart.jsx
@@ -1,7 +1,7 @@
 // import LineChart from "@/components/lineGraph"
 import dynamic from "next/dynamic";
 import { useUserContext } from "@/components/datacontext";
-import Link from "next/link";
+import Navbar from "@/components/navbar";
 
 const Graph = () => {
   const DynmaicComp = dynamic(() => import("../components/lineGraph"), {
@@ -11,13 +11,8 @@ const Graph = () => {
   // console.log(`userdata from context`, userData);
   return (
     <>
-      <section className="m-5">
-        <Link href='/'><button
-          type="submit"
-          className="mt-3 text-white bg-[#6469ff] font-medium  rounded-md text-sm sm:w-24 xs:w-full px-5 py-2.5 text-center"
-        >
-          back
-        </button></Link>
+      <section>
+        <Navbar />
         {userData.length !== 0 ? <DynmaicComp data={userData} /> : (
           <p>no data found</p>
         )}
